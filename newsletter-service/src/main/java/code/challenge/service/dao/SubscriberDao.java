427db6b224a317b5dcd05f5b73c5d6f9f7403ff1
juanjoal <35507699+juanjoal@users.noreply.github.com>
2018-01-16 23:28:28 +0100

    initial commit

---
+++ newsletter-service/src/main/java/code/challenge/service/dao/SubscriberDao.java
@@ -0,0 +1,62 @@
+package code.challenge.service.dao;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import code.challenge.service.model.Subscriber;
+
+public class SubscriberDao {
+	
+	private static Logger logger = LoggerFactory.getLogger(SubscriberDao.class);
+	
+	
+	private String insert_sp = "{call insert_subscriptor(?,?, ?, ?, ?, ?, ?) }";
+	
+
+
+	public Subscriber create(Subscriber subscriber, short idNewsletter, short idCampaing) {
+
+		int outputValue = -1;
+		Context initContext;
+		try {
+			initContext = new InitialContext();	
+			DataSource ds = (DataSource)initContext.lookup("java:jboss/MySqlDS");
+			
+			//autoclose
+			try (Connection conn = ds.getConnection();
+					CallableStatement insertSubscriptor = conn.prepareCall(insert_sp)){
+				
+				insertSubscriptor.setString(1, subscriber.getEmail());
+				insertSubscriptor.setString(2, subscriber.getFirstname());
+				insertSubscriptor.setString(3, subscriber.getGender());
+				insertSubscriptor.setDate(4, new java.sql.Date(subscriber.getDateOfBirth().getTime()));
+				insertSubscriptor.setShort(5, idNewsletter);
+				insertSubscriptor.setShort(6, idCampaing);
+				insertSubscriptor.registerOutParameter(7, Types.SMALLINT);
+				boolean resultado = insertSubscriptor.execute();
+				outputValue = insertSubscriptor.getShort(7);
+				subscriber.setId(outputValue);
+				System.out.println("outputValue: " + outputValue);
+				System.out.println("resultado: " + resultado);
+			}catch (SQLException e) {
+				logger.error("", e);
+			}
+
+
+		} catch (NamingException e) {
+			logger.error("", e);
+		} 
+		return subscriber;
+	}
+	
+}
---
+++ newsletter-service/src/main/java/code/challenge/service/rest/SubscriberEndpoint.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package code.challenge.service.rest;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import code.challenge.service.dao.SubscriberDao;
+import code.challenge.service.model.Subscriber;
+
+/**
+ * @author Juanjo
+ *
+ */
+@RequestScoped
+@Path("/subscribers")
+@Produces({ "application/xml", "application/json" })
+@Consumes({ "application/xml", "application/json" })
+public class SubscriberEndpoint {
+
+	/**
+	* @param subscriber
+	* @return
+	*/
+	@POST
+	public Response create(@HeaderParam("X-My-API-Key-Token") String apikeytoken , final Subscriber subscriber) {
+		
+		System.out.println("apikeytoken: " + apikeytoken);
+		System.out.println("subscriber: " + subscriber);
+		if (isValidToken(apikeytoken)) {
+			SubscriberDao dao = new SubscriberDao();
+			Subscriber subscriberPersisted = dao.create(subscriber, (short) 1, (short) 1);
+			return Response.ok(subscriberPersisted).build();
+		}else {
+			return Response.status(Response.Status.UNAUTHORIZED)
+			        .type(MediaType.TEXT_PLAIN_TYPE)
+			        .entity("Unauthorized")
+			        .build();
+		}
+		
+
+	}
+	
+	/**
+	 * Mock method
+	 * 
+	 * @param apikeytoken
+	 * @return
+	 */
+	private boolean  isValidToken(String apikeytoken) {
+		if (apikeytoken != null && apikeytoken.equals("01234567890ABCDEFGH")) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+
+}
